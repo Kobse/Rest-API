27406332608ad78b55ebf62e2ccf5e68016932d8
kobse <j.s.jakobsen@hotmail.com>
2021-09-24 10:54:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+// Routes
+app.get('/', (req, res) => {
+	res.send('We are on home');
+});
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+	console.log('connected to DB!')
+);
+
+// Listen to server localhost:3000
+app.listen(3000);
